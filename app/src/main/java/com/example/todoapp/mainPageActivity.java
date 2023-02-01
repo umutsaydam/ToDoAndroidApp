98d16def5598ecc47183a24bc59a66ea7f92e85a
Umut SAYDAM <umutsaydam24@gmail.com>
2023-02-01 14:02:46 +0300

    nav handled

--- app/src/main/java/com/example/todoapp/mainPageActivity.java
+++ app/src/main/java/com/example/todoapp/mainPageActivity.java
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -36,18 +40,21 @@ public class mainPageActivity extends AppCompatActivity {
         toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2);
+        NavController navController = navHostFragment.getNavController();
         navigationView = findViewById(R.id.mainPageNavigation);
         navigationView.bringToFront();
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId() == R.id.logout){
-                    FirebaseAuth.getInstance().signOut();
-                    startActivity(new Intent(mainPageActivity.this, MainActivity.class));
-                }
-                return false;
+        navigationView.setNavigationItemSelectedListener(item -> {
+            if(navController.getCurrentDestination().getId() != R.id.mainPageFragment && item.getItemId() == R.id.toDoMenu){
+                navController.navigate(R.id.action_challengesFragment_to_mainPageFragment);
+            }else if(navController.getCurrentDestination().getId() != R.id.challengesFragment && item.getItemId() == R.id.challengeMenu){
+                navController.navigate(R.id.action_mainPageFragment_to_challengesFragment);
+            }else if(item.getItemId() == R.id.logout){
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(mainPageActivity.this, MainActivity.class));
             }
+            return false;
         });
     }
 
