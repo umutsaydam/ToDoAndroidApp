efb525735809098953e35809c1a6ea3295e3bf57
Umut SAYDAM <umutsaydam24@gmail.com>
2023-02-05 11:24:37 +0300

    adapter param setted

--- app/src/main/java/com/example/todoapp/Adapters/ChallengeLayerAdapter.java
+++ app/src/main/java/com/example/todoapp/Adapters/ChallengeLayerAdapter.java
@@ -1,16 +1,32 @@
 package com.example.todoapp.Adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.todoapp.Models.ChallengeModel;
+import com.example.todoapp.R;
+
+import java.util.ArrayList;
+
 public class ChallengeLayerAdapter extends RecyclerView.Adapter<ChallengeLayerAdapter.ChallengeLayerHolder> {
+    private ArrayList<ChallengeModel> challengeModels;
+    private Context context;
+
+    public ChallengeLayerAdapter(ArrayList<ChallengeModel> challengeModels, Context context) {
+        this.challengeModels = challengeModels;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public ChallengeLayerAdapter.ChallengeLayerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.to_do_layer, parent, false);
+        return new ChallengeLayerHolder(view);
     }
 
     @Override
@@ -20,7 +36,7 @@ public class ChallengeLayerAdapter extends RecyclerView.Adapter<ChallengeLayerAd
 
     @Override
     public int getItemCount() {
-        return 0;
+        return challengeModels.size();
     }
 
     public class ChallengeLayerHolder extends RecyclerView.ViewHolder {
