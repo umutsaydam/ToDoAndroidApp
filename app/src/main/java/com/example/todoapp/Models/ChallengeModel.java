756d2adb4ea19836393a343cf60985379b550614
Umut SAYDAM <umutsaydam24@gmail.com>
2023-02-16 12:27:11 +0300

    setted

--- app/src/main/java/com/example/todoapp/Models/ChallengeModel.java
+++ app/src/main/java/com/example/todoapp/Models/ChallengeModel.java
@@ -1,30 +1,48 @@
 package com.example.todoapp.Models;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ChallengeModel {
     String id;
     String challengeTitle;
     String challengeCategory;
-    int challengeDay;
+    String startDay;
+    String endDay;
     String challengeDescription;
+    int challengeDay;
     List<Boolean> challangeStatus;
 
     public ChallengeModel() {
     }
 
-    public ChallengeModel(String id, String challengeTitle, String challengeCategory, int challengeDay, String challengeDescription) {
+    public ChallengeModel(String id, String challengeTitle, String challengeCategory, String startDay, String endDay, String challengeDescription) {
         this.id = id;
         this.challengeTitle = challengeTitle;
         this.challengeCategory = challengeCategory;
-        this.challengeDay = challengeDay;
+        this.startDay = startDay;
+        this.endDay = endDay;
         this.challengeDescription = challengeDescription;
-        Boolean [] tmp = new Boolean[challengeDay];
-        Arrays.fill(tmp, false);
-        this.challangeStatus = new ArrayList<Boolean>(Arrays.asList(tmp));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date s = sdf.parse(startDay);
+            Date e = sdf.parse(endDay);
+            long diff = e.getTime() - s.getTime();
+            this.challengeDay = (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+            boolean [] tmp = new boolean[challengeDay];
+            Arrays.fill(tmp, false);
+            this.challangeStatus = new ArrayList<Boolean>(Arrays.asList(tmp));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
@@ -32,20 +50,48 @@ public class ChallengeModel {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getChallengeTitle() {
         return challengeTitle;
+    }
+
+    public void setChallengeTitle(String challengeTitle) {
+        this.challengeTitle = challengeTitle;
     }
 
     public String getChallengeCategory() {
         return challengeCategory;
     }
 
-    public int getChallengeDay() {
-        return challengeDay;
+    public void setChallengeCategory(String challengeCategory) {
+        this.challengeCategory = challengeCategory;
+    }
+
+    public String getStartDay() {
+        return startDay;
+    }
+
+    public void setStartDay(String startDay) {
+        this.startDay = startDay;
+    }
+
+    public String getEndDay() {
+        return endDay;
+    }
+
+    public void setEndDay(String endDay) {
+        this.endDay = endDay;
     }
 
     public String getChallengeDescription() {
         return challengeDescription;
+    }
+
+    public void setChallengeDescription(String challengeDescription) {
+        this.challengeDescription = challengeDescription;
     }
 
     public List<Boolean> getChallangeStatus() {
