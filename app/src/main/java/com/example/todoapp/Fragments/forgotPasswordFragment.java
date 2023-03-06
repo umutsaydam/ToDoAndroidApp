36d5ebe65145249f0c889e05b8df3bd133673150
Umut SAYDAM <umutsaydam24@gmail.com>
2023-03-06 18:44:48 +0300

    added sending email for reset password

--- app/src/main/java/com/example/todoapp/Fragments/forgotPasswordFragment.java
+++ app/src/main/java/com/example/todoapp/Fragments/forgotPasswordFragment.java
@@ -2,65 +2,57 @@ package com.example.todoapp.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.todoapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link forgotPasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class forgotPasswordFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public forgotPasswordFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment forgotPasswordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static forgotPasswordFragment newInstance(String param1, String param2) {
-        forgotPasswordFragment fragment = new forgotPasswordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    private EditText editTxtEmail;
+    private Button btnSendEmail;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false);
+        View view = inflater.inflate(R.layout.fragment_forgot_password, container, false);
+        
+        editTxtEmail = view.findViewById(R.id.editTxtEmail);
+        btnSendEmail = view.findViewById(R.id.btnSendEmail);
+        btnSendEmail.setOnClickListener(view1 -> {
+            String mail = editTxtEmail.getText().toString();
+            if(!mail.isEmpty()){
+                FirebaseAuth.getInstance().sendPasswordResetEmail(mail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getContext(), R.string.sent_email_repassword, Toast.LENGTH_SHORT).show();
+                            Navigation.findNavController(view).navigate(R.id.action_forgotPasswordFragment_to_signInFragment);
+                        }else{
+                            Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        
+        return view;
     }
 }
