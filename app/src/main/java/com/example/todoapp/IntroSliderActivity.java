e754a76d755e36fa2838f23efc8c7dcc932c2b2a
Umut SAYDAM <umutsaydam24@gmail.com>
2023-05-12 21:59:36 +0300

    lang selection added

--- app/src/main/java/com/example/todoapp/IntroSliderActivity.java
+++ app/src/main/java/com/example/todoapp/IntroSliderActivity.java
@@ -1,11 +1,13 @@
 package com.example.todoapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -15,6 +17,7 @@ import com.example.todoapp.Fragments.slider2Fragment;
 import com.example.todoapp.Fragments.slider3Fragment;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class IntroSliderActivity extends AppCompatActivity {
     private Button btnSlider;
@@ -33,6 +36,13 @@ public class IntroSliderActivity extends AppCompatActivity {
         fragments.add(new slider1Fragment());
         fragments.add(new slider2Fragment());
         fragments.add(new slider3Fragment());
+
+        SharedPreferences preferences = getSharedPreferences("Settings.Lang", MainActivity.MODE_PRIVATE);
+        String lan = preferences.getString("Lang", "");
+        if (lan.equals(""))
+            showLanguageDialog();
+        else
+            setLocale(lan);
 
         SliderAdapter adapter = new SliderAdapter(getSupportFragmentManager(), getLifecycle());
         adapter.setFragments(fragments);
@@ -59,9 +69,38 @@ public class IntroSliderActivity extends AppCompatActivity {
 
     }
 
+    private void showLanguageDialog() {
+        final String [] langs = {"EN", "TR"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(IntroSliderActivity.this);
+        mBuilder.setTitle(R.string.select_a_language);
+        mBuilder.setSingleChoiceItems(langs, -1, (dialogInterface, i) -> {
+            if(i == 0){
+                setLocale(langs[i]);
+                recreate();
+            }else if(i == 1){
+                setLocale(langs[i]);
+                recreate();
+            }
+            dialogInterface.dismiss();
+        });
+        AlertDialog alertDialog = mBuilder.create();
+        alertDialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getSharedPreferences("Settings.Lang", MODE_PRIVATE).edit();
+        editor.putString("Lang", lang.isEmpty() ? "EN": lang);
+        editor.apply();
+    }
+
     public void sliderListener(int pageNum){
         if (pageNum+1 == fragments.size()){
-            btnSlider.setText("Done !");
+            btnSlider.setText(getString(R.string.done));
         }else{
             btnSlider.setText(R.string.next);
         }
--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -20,42 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SharedPreferences preferences = getSharedPreferences("Settings.Lang", MainActivity.MODE_PRIVATE);
-        String lan = preferences.getString("Lang", "");
-        if (lan.equals(""))
-            showLanguageDialog();
-        else
-            setLocale(lan);
-    }
-
-    private void showLanguageDialog() {
-        final String [] langs = {"EN", "TR"};
-        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
-        mBuilder.setTitle(R.string.select_a_language);
-        mBuilder.setSingleChoiceItems(langs, -1, (dialogInterface, i) -> {
-            if(i == 0){
-                setLocale(langs[i]);
-                recreate();
-            }else if(i == 1){
-                setLocale(langs[i]);
-                recreate();
-            }
-            dialogInterface.dismiss();
-        });
-        AlertDialog alertDialog = mBuilder.create();
-        alertDialog.show();
-    }
-
-    private void setLocale(String lang) {
-        Locale locale = new Locale(lang);
-        Locale.setDefault(locale);
-        Configuration config = new Configuration();
-        config.setLocale(locale);
-        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-        SharedPreferences.Editor editor = getSharedPreferences("Settings.Lang", MODE_PRIVATE).edit();
-        editor.putString("Lang", lang.isEmpty() ? "EN": lang);
-        editor.apply();
     }
 
     @Override
